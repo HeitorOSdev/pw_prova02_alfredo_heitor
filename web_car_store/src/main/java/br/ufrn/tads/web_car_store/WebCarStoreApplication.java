3d4bbc14571c503212c1902ffbceaae499dbeb81
HeitorOtaciano <164053220+HeitorOtaciano@users.noreply.github.com>
2025-06-08 13:29:19 -0300

    Arquivo Spring Boot inicializado com as dependências necessárias

---
+++ web_car_store/src/main/java/br/ufrn/tads/web_car_store/WebCarStoreApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.tads.web_car_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCarStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCarStoreApplication.class, args);
+	}
+
+}
---
+++ web_car_store/src/test/java/br/ufrn/tads/web_car_store/WebCarStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.tads.web_car_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCarStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
